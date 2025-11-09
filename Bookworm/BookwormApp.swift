ee34497a08fcb3d567f28923ef4633dddec4d2b3
ra1z <purnamaanrai7@gmail.com>
2025-11-09 08:52:57 +0545

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Purnaman Rai (College) on 09/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
